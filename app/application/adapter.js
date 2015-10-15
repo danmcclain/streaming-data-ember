e1488e1a337fa771e7e310e2e22ebadbf43a6911
Dan McClain <git@danmcclain.net>
2015-10-15 09:51:07 -0400

    Add ChannelProxy to promisify channel interactions

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -22,13 +22,13 @@ export default DS.Adapter.extend({
     const channel = get(this, 'channel');
 
     if (channel) {
-      return new RSVP.Promise((resolve, reject) => {
-        channel.push('create', data)
-        .receive('ok', (payload) => { resolve(payload); })
-        .receive('error', (payload) => { reject(payload); });
-      });
+      return channel.push('create', data);
     } else {
     }
+  },
+
+  findRecord(store, type, id, snapshot) {
+    debugger;
   },
 
   findAll(store, type, sinceToken) {
@@ -36,31 +36,17 @@ export default DS.Adapter.extend({
     var serializer = store.serializerFor(type.modelName);
     const phoenix = get(this, 'phoenix');
 
-    return get(phoenix, 'socket')
-      .then((socket) => {
-        const promise = new RSVP.Promise((resolve, reject) => {
-          const channel = socket.channel("contacts:index", {});
-          channel.on('new_contact', (payload) => {
-            payload = serializer._normalizeResponse(null, null, payload);
-            store.push(payload);
-          });
-
-          channel.join().receive('ok', ({contacts}) => {
-            resolve(channel);
-          }).receive('error', () => {
-            reject('errored');
-          });
-        })
-        .then((channel) => {
-          const promise = new RSVP.Promise((resolve, reject) => {
-            set(adapter, 'channel', channel);
-            channel.push('all')
-            .receive('ok', (payload) => { resolve(payload) })
-            .receive('error', () => { reject('Sent all unsucessfully') });
-          })
-          return promise;
+    return phoenix.channel('contacts:index')
+      .then((channel) => {
+        channel.on('new_contact', (payload) => {
+          payload = serializer._normalizeResponse(null, null, payload);
+          store.push(payload);
         });
-        return promise;
+        return channel.join();
+      })
+      .then((channel) => {
+        set(adapter, 'channel', channel);
+        return channel.push('all')
       });
   }
 });
--- app/services/phoenix.js
+++ app/services/phoenix.js
@@ -4,16 +4,58 @@ import { Socket } from 'phoenix';
 const {
   RSVP,
   Service,
+  get,
   on,
   set
 } = Ember;
+
+const { Promise } = RSVP;
+
+const ChannelProxy = Ember.Object.extend({
+  init(channel) {
+    set(this, 'channel', channel);
+  },
+
+  join() {
+    return new Promise((resolve, reject) => {
+      const channel = get(this, 'channel');
+      channel.join()
+        .receive('ok', () => {
+          resolve(this);
+        })
+        .receive('error', (payload) => {
+          reject(payload);
+        });
+    });
+  },
+
+  on(event, callback) {
+    get(this, 'channel').on(event, callback);
+  },
+
+  off(event) {
+    get(this, 'channel').off(event);
+  },
+
+  push(event, payload) {
+    return new Promise((resolve, reject) => {
+      get(this, 'channel').push(event, payload)
+        .receive('ok', (payload) => {
+          resolve(payload);
+        })
+        .receive('error', (payload) => {
+          reject(payload);
+        });
+    });
+  }
+});
 
 export default Service.extend({
   socket: null,
   connect: on('init', function() {
     const socket = new Socket('/socket');
 
-    const promise = new RSVP.Promise(function(resolve, reject) {
+    const promise = new RSVP.Promise(function(resolve) {
       socket.onOpen(() => {
         resolve(socket);
       });
@@ -21,5 +63,12 @@ export default Service.extend({
     socket.connect();
 
     set(this, 'socket', promise);
-  })
+  }),
+
+  channel(channelName, options) {
+    return get(this, 'socket').then((socket) => {
+      const channel = socket.channel(channelName, options);
+      return new ChannelProxy(channel);
+    });
+  }
 });
