4636040ae3f91e988e2dc04004c68654e405a2a9
Dan McClain <git@danmcclain.net>
2016-05-15 18:56:19 -0400

    Delete

--- app/contacts/index/route.js
+++ app/contacts/index/route.js
@@ -6,6 +6,6 @@ const {
 
 export default Route.extend({
   model() {
-    return this.store.findAll('contact', { scope: 'global', subscribeTo: ['new', 'update'] });
+    return this.store.findAll('contact', { scope: 'global', subscribeTo: ['new', 'update', 'delete'] });
   }
 })
